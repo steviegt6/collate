73b7b0fa7cb94176ff149c43c172d68e4438638d
Tomat <xxlennygamerxx@gmail.com>
2023-07-04 20:27:25 -0700

    Revert "bye"
    This reverts commit d408ad1414fd1c82d6ed565b4fb224231a61896e.

---
+++ publish.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+dotnet run --project "./src/Tomat.Collate.LocalPublish/Tomat.Collate.LocalPublish.csproj" -c "Release"
---
+++ src/Tomat.Collate.AccessTransformer/TransformerTask.cs
@@ -0,0 +1,9 @@
+﻿using Tomat.Collate.NuGet.Shared;
+
+namespace Tomat.Collate.AccessTransformer;
+
+public sealed class TransformerTask : CollateTask {
+    protected override bool Execute(CollateContext ctx) {
+        throw new System.NotImplementedException();
+    }
+}
